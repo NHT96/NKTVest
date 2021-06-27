9d607e0bace96bb8c6a15ea86f34e09b5bc715d1
Hữu Tài <nguyenhuutai.251096@gmail.com>
2021-06-27 11:36:53 +0700

    Initial commit

---
+++ NKTVest/Controllers/VestController.cs
@@ -0,0 +1,39 @@
+﻿using NKTVest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NKTVest.Controllers
+{
+    public class VestController : Controller
+    {
+        NKTVDataContext data = new NKTVDataContext();
+        // GET: Vest
+        private List<SANPHAM> DSSPmoi(int n)
+        {
+            return data.SANPHAMs.OrderByDescending(a => a.NGAYTHEM).Take(n).ToList();
+        }
+        public ActionResult Index()
+        {
+            var spm = DSSPmoi(10);
+            return View(spm);
+        }
+        public ActionResult LoaiSP()
+        {
+            var lsp = from l in data.LOAISPs select l;
+            return PartialView(lsp);
+        }
+        public ActionResult SPTheoLoai(string id)
+        {
+            var sp = from a in data.SANPHAMs where a.MALOAISP==id select a;
+            return View(sp);
+        }
+        public ActionResult Details(string id)
+        {
+            var sp = from a in data.SANPHAMs where a.MASP == id select a;
+            return View(sp.Single());
+        }
+    }
+}
